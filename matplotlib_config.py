f78b7569f879abd0e168c2deccfd156af78eef44
Frederik Wilde <42576579+frederikwilde@users.noreply.github.com>
2024-10-17 11:57:31 +0200

    Create matplotlib_config.py

---
+++ matplotlib_config.py
@@ -0,0 +1,8 @@
+import matplotlib as mpl
+
+
+mpl.rcParams['font.family'] = 'Times New Roman'
+mpl.rcParams['font.size'] = 14
+mpl.rcParams['mathtext.fontset'] = 'cm'
+mpl.rcParams['xtick.labelsize'] = 10
+mpl.rcParams['ytick.labelsize'] = 10
